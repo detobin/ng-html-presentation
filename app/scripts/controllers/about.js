bbe7c236d364fe62721bb2d6646718f1e7b78dba
Donal Tobin <donal.tobin@sap.com>
2016-01-24 17:27:47 +0000

    Yeoman angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngHtmlPresentationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngHtmlPresentationApp
+ */
+angular.module('ngHtmlPresentationApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngHtmlPresentationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngHtmlPresentationApp
+ */
+angular.module('ngHtmlPresentationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
